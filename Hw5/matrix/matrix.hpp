89ef555fc83eec6a34ffb8f64f3aeb1dc21b19a1
Giovanny-HR <1021941hr@gmail.com>
2022-09-21 20:25:41 +0200

    Hw5 Added!

---
+++ Hw5/matrix/matrix.hpp
@@ -0,0 +1,48 @@
+
+//-------------------------------------------------------------
+
+#ifndef HW5_MATRIX_HPP
+#define HW5_MATRIX_HPP
+
+#endif //HW5_MATRIX_HPP
+//-------------------------------------------------------------
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+namespace st = std;
+
+namespace matrix {
+
+    template <class T>
+    class Matrix {
+    public:
+        Matrix (int nrOfRows = 0, int nrOfColumns = 0);
+        Matrix (int nrOfRows, st::vector <T> const &entries);
+        // Matrix (Matrix const &matrix);   // Uncomment to check copy elision
+        T &access (int rowIndex, int columnIndex);
+        void print (st::ostream &outputStream);
+        int getEntriesSize ();
+
+    protected:
+        int nrOfRows;
+        int nrOfColumns;
+        st::vector <T> entries;
+
+        template <class U>
+        friend Matrix <U> add (Matrix <U> &matrix0, Matrix <U> &matrix1);
+
+        template <class U>
+        friend Matrix <U> multiply (Matrix <U> &matrix0, Matrix <U> &matrix1);
+
+        template <class U, class V>
+        friend Matrix <U> multiply (V scalar, Matrix <U> &matrix);
+
+        template <class U, class V>
+        friend Matrix <U> multiply (Matrix <U> &matrix, V scalar);
+    };
+
+#include "matrix.cpp"
+
+}
