eb3aab30e8abe82d7e7dca85bd6bd1c4a21b2696
Giovanny-HR <1021941hr@gmail.com>
2022-09-21 20:30:00 +0200

    Hw6 Added!

---
+++ Hw6/matrixpp/matrixpp.hpp
@@ -0,0 +1,47 @@
+
+#ifndef HW6_MATRIXPP_HPP
+#define HW6_MATRIXPP_HPP
+
+#endif //HW6_MATRIXPP_HPP
+
+#pragma once
+
+#include <iomanip>
+#include <iostream>
+#include <vector>
+namespace st = std;
+
+namespace matrix {
+
+    template <class T>
+    class Matrix {
+    public:
+        Matrix (int nrOfRows = 0, int nrOfColumns = 0);
+        Matrix (int nrOfRows, st::vector <T> const &entries);
+        T &operator () (int rowIndex, int columnIndex);
+        int getEntriesSize ();
+
+    protected:
+        int nrOfRows;
+        int nrOfColumns;
+        st::vector <T> entries;
+
+        template <class U>
+        friend st::ostream &operator << (st::ostream &outputStream, Matrix <U> &matrix);
+
+        template <class U>
+        friend Matrix <U> operator + (Matrix <U> &matrix0, Matrix <U> &matrix1);
+
+        template <class U>
+        friend Matrix <U> operator * (Matrix <U> &matrix0, Matrix <U> &matrix1);
+
+        template <class U, class V>
+        friend Matrix <U> operator * (V scalar, Matrix <U> &matrix);
+
+        template <class U, class V>
+        friend Matrix <U> operator * (Matrix <U> &matrix, V scalar);
+    };
+
+#include "matrixpp.cpp"
+
+}
